deb11ccfd58b7e37075b5fb8e48086c286ad8797
Ayush-25-27 <Ayushranj09912@gmail.com>
2026-08-15 21:36:36 +0530

    Time: 4 ms (100%), Space: 46.2 MB (6%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        long original = x;
+        long reversed = 0;
+        while (x != 0) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+        return original == reversed;
+    }
+} 
+ 
